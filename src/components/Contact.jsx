f50194c611657c65fddaafafe950b1bea6a22cef
geetamatkar <geetamatkar1@gmail.com>
2023-12-02 18:01:17 -0600

    # Updated contact.js

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect , useState } from 'react';
 import styles from '../style';
 
+
 const Contact = () => {
+  const [userPin, setUserPin] = useState('');
+  const [userCoordinates, setUserCoordinates] = useState(null);
 
     //const googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
     const googleMapsApiKey = import.meta.env.REACT_APP_GOOGLE_MAPS_API_KEY;
@@ -10,11 +13,36 @@ const Contact = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Logic to handle form submission (e.g., sending email) goes here
-        // You might use an API call or a backend service to send the email
-        // For now, let's log the form data to the console
+        
         console.log('Form submitted:', e.target.emailContent.value);
       };
+
+      const handlePincodeChange = (e) => {
+        setUserPin(e.target.value);
+      };
+
+      const handlePincodeSubmit = (e) => {
+        e.preventDefault();
+    
+        // Use Google Geocoding API to convert pin code to coordinates
+        fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${userPin}&key=AIzaSyAzHKLQPg2XBB_2nfWmcl0MEzaYesitNzU&`)
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.results && data.results.length > 0) {
+              const { lat, lng } = data.results[0].geometry.location;
+              setUserCoordinates({ lat, lng });
+            } else {
+              // Handle error when pin code does not return valid coordinates
+              console.error('Invalid pin code or unable to retrieve coordinates.');
+            }
+          })
+          .catch((error) => {
+            console.error('Error fetching coordinates:', error);
+          });
+      };
+    
+
+
 
 
   useEffect(() => {
@@ -37,7 +65,7 @@ const Contact = () => {
       }
     };
 
-    window.initMap = () => {
+    /*window.initMap = () => {
       // Initialize Google Maps
       const locations = [
         { lat: 40.7128, lng: -74.006 },
@@ -48,25 +76,61 @@ const Contact = () => {
         { lat: 39.125554, lng: -84.53485 },
         { lat: 39.81927, lng: -89.61977 },
         // Add more locations as needed
-      ];
+      ];*/
 
+      const initMap = () => {
+        const map = new window.google.maps.Map(document.getElementById('map'), {
+          center: userCoordinates || { lat: 41.83499, lng: -87.62704 }, // Default to a location if user's coordinates are not available
+          zoom: 6,
+        });
+  
+      /*
       const map = new window.google.maps.Map(document.getElementById('map'), {
         center: { lat: 41.83499, lng: -87.62704 },
         zoom: 6,
-      });
+      });*/
 
       // Create markers for each location
-      locations.forEach((location, index) => {
+      /*locations.forEach((location, index) => {
         new window.google.maps.Marker({
           position: location,
           map,
           title: `Location ${index + 1}`,
         });
-      });
+      });*/
+
+      if (userCoordinates) {
+        // Show marker for user's location
+        new window.google.maps.Marker({
+          position: userCoordinates,
+          map,
+          title: 'Your Location',
+        });
+
+        // You can add logic to find nearby locations based on userCoordinates
+        // For example:
+        const nearbyLocations = [
+          // Define nearby locations based on user's coordinates
+          // This can be fetched from an API or a predefined list
+          { lat: userCoordinates.lat + 0.1, lng: userCoordinates.lng + 0.1 },
+          { lat: userCoordinates.lat - 0.1, lng: userCoordinates.lng - 0.1 },
+          // Add more nearby locations as needed
+        ];
+
+        // Create markers for nearby locations
+        nearbyLocations.forEach((location, index) => {
+          new window.google.maps.Marker({
+            position: location,
+            map,
+            title: `Nearby Location ${index + 1}`,
+          });
+        });
+      }
+    
     };
 
     loadGoogleMapsScript();
-  }, []);
+  }, [userCoordinates]);
 
 
   return (
@@ -100,6 +164,31 @@ const Contact = () => {
     </div>
     <div className={`flex-1 ${styles.flexCenter} ${styles.marginY}`}> 
      
+      
+
+      {/* */}
+
+      <form onSubmit={handlePincodeSubmit} className="mt-8">
+        <h3 className="text-white m-3">Find Locations Near You</h3>
+        <label htmlFor="pincode" className="text-white m-3">
+          Enter your pin code:
+        </label>
+        <input
+          type="text"
+          id="pincode"
+          name="pincode"
+          value={userPin}
+          onChange={handlePincodeChange}
+          className="border p-2 rounded focus:outline-none focus:ring focus:border-blue-300 w-2/3 m-3"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 m-3 mt-4 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+        >
+          Find Locations
+        </button>
+      </form>
+
       <div id="map" style={{ width: '100%', height: '400px', marginTop: '20px' }}></div>
       </div>
     
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -71,9 +71,27 @@ const Navbar = () => {
         {navLinks.map((nav, index) => {
           if ((nav.title === "Data Analytics" && username !== 'admin') || 
               (nav.title === "Login" && isLoggedIn) || 
-              (nav.title === "Logout" && !isLoggedIn)) {
+              (nav.title === "Logout" && !isLoggedIn) ||
+              (nav.title === "Profile" && !isLoggedIn)) {
             return null;
           }
+
+          if (nav.title === "Profile" && isLoggedIn) {
+            return (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                  active === nav.title ? "text-white" : "text-dimWhite"
+                } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+                onClick={() => handleNavLinkClick(nav.title)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            );
+          }
+
+
+
           return (
             <li
               key={nav.id}
